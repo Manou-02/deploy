0e81e0efffd9b284eafc4e37254c825b639430ee
Manou-02 <mrakotoarivelo97@gmail.com>
2022-02-09 15:04:58 +0100

    deploy essai

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './Pages/About';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+          <span>
+            <Link to="/">Home</Link>
+          </span>
+          <span>
+            <Link to="/about">About</Link>
+          </span>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+      </Router>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
